44826518e3da6224d6e746e36715b2c40790cdcf
amm <17629268080@163.com>
2022-10-06 09:03:56 +0800

    2022年10月6日09点03分

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,32 +2,21 @@ const head = require('./config/head.js');
 const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
+
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+
   title: "Amm",
-  description: 'vdoing博客主题模板',
+  description: '💻学习📝记录🔗分享\n学无止境是永远前进的基础，跃然纸上是对知识的总结交代，与众分享则是实现价值的最好方式。',
   base: '/notes/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
+    extractHeaders: [ 'h2', 'h3', 'h4', 'h5', 'h6' ]
   },
 
+
   head,
-  // vssue 评论插件
-  plugins: [
-    [
-      "vuepress-plugin-vssue-global",
-      {
-        platform: "github",
-        title: "[Comment]<%- frontmatter.title %>",
-        needComments: true,
-        // 其他的 Vssue 配置
-        autoCreateIssue: true,
-        clientId: "2481bb1a1a980484fc80",
-        clientSecret: "77a3471d39d6c05ffa25504c0aa73f14cd19b0ae",
-        owner: "AmmGo",
-        repo: "notes",
-      },
-    ],
-  ],
+  plugins,
   themeConfig,
 }
