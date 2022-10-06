45b1ea8c2dfc7266ad65e8a4510821281c239549
amm <17629268080@163.com>
2022-10-06 09:11:12 +0800

    2022年10月6日09点11分

--- docs/.vuepress/config/themeConfig/nav.js
+++
@@ -1,9 +0,0 @@
-// nav
-module.exports = [
-  { text: '首页', link: '/' },
-  { text: '终身学习',link: '/learn/',},
-  { text: '电影音乐',link: '/av/',},
-  { text: '生活随笔', link: '/notes/' },
-  { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://ammgo.github.io/notes/' },
-]
